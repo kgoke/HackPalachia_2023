2dfee1dc570b74a9a39f4c9c83acd34595603dfd
Jansen Craft <89416543+jc103820@users.noreply.github.com>
2023-03-04 21:18:58 -0500

    flip should work

--- Econeer/Player.cs
+++ Econeer/Player.cs
@@ -9,24 +9,46 @@ public class Player : KinematicBody2D{
 	float maxFallSpeed = 1000;
 	float minFallSpeed = 5;
 	
+	float jumpForce = 1000;
+	
+	AnimatedSprite animatedSprite;
+	
+	
 	public override void _Ready(){
-		
+		animatedSprite = (AnimatedSprite)GetNode("Player/AnimatedSprite");
+	
 	}
 
-	public override void PhysicsProcess(float delta){
-		//player gravity
+	public override void _PhysicsProcess(float delta){
+		// player gravity
 		direction.y += gravity;
 		if(direction.y > maxFallSpeed){
 			direction.y = maxFallSpeed;
 		}
 		
-		if(isOnFloor()){
+		if(IsOnFloor()){
 			direction.y = minFallSpeed;
 		}
 		
-		//player movements
+		// player movements
 		direction.x = Input.GetActionStrength("move_right")-Input.GetActionStrength("move_left");
 		direction.x *= movementSpeed;
+		
+		// jump
+		if(IsOnFloor() && Input.IsActionJustPressed("jump")){
+			direction.y =- jumpForce;
+		}
+		
+		// flip sprite
+		if(direction.x > 0){
+			animatedSprite.FlipH = false;
+		} else if(direction.x < 0){
+			animatedSprite.FlipH = true;
+		}
+		
+		// play animatio 
+		
+		
 		direction = MoveAndSlide(direction, Vector2.Up);
 	}
 }
